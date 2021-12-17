e7c918e8a3c73f12768f5c444b880bda0b6d6b7d
Varsha Jain <csvarshajain@gmail.com>
2021-12-17 00:48:12 -0500

    added home page and able to render post

---
+++ routes/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const sequelize = require('../config/connection')
+
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'content', 'user_id', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    }).then(dbPostData => {
+        // pass a single post object into the homepage template
+        // console.log(dbPostData[0])
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
+        // res.json(dbPostData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        })
+})
+
+module.exports = router;
